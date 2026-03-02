8b5bed5727aeea30a441c6ae8c3aefa5c292c423
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2026-03-03 02:29:15 +0600

    updated sarver deply api

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
